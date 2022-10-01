4734619a1c0dba018773e6a0cc0c16ec27ebbf0f
Andreas Thomas <dev@chronark.com>
2022-10-01 22:36:48 +0200

    wip

--- svc/web/pages/[teamSlug]/index.tsx
+++ svc/web/pages/[teamSlug]/index.tsx
@@ -2,11 +2,15 @@ import { Layout } from "../../components/app/layout/nav";
 import { useUser } from "@clerk/nextjs";
 
 import React from "react";
+import { useRouter } from "next/router";
 
 export default function Teampage() {
     const { user } = useUser();
 
     const breadcrumbs = user?.username ? [] : [];
+    const router = useRouter()
+
+    router.push(router.asPath + "/endpoints")
 
     return (
         <Layout breadcrumbs={breadcrumbs}>
