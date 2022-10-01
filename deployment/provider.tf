034f906b017b0c14600d5c353c4a4547e8f682b3
Andreas Thomas <dev@chronark.com>
2022-10-01 13:58:01 +0200

    wip

--- deployment/provider.tf
+++ deployment/provider.tf
@@ -4,19 +4,20 @@ terraform {
       source  = "vercel/vercel"
       version = ">=0.8.0"
     }
-  }
-  upstash = {
-    source  = "upstash/upstash"
-    version = "1.1.5"
-  }
-
-
-
-  provider "upstash" {
-    email = var.upstash_email
-    api_key = var.upstash_api_key
+    upstash = {
+      source  = "upstash/upstash"
+      version = "1.1.5"
+    }
   }
 }
+
+
+
+provider "upstash" {
+  email   = var.upstash_email
+  api_key = var.upstash_api_key
+}
+
 
 provider "vercel" {
   api_token = var.vercel_token
