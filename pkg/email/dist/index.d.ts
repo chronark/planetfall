a4642fd53895ed07c22658732eda2d27db052e7f
Andreas Thomas <dev@chronark.com>
2022-10-02 17:02:09 +0200

    wip

--- pkg/email/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Email {
-    private client;
-    constructor();
-    sendOTP(email: string, otp: string): Promise<void>;
-}
-//# sourceMappingURL=index.d.ts.map
--- svc/scheduler/dist/events.d.ts
+++
@@ -1,8 +0,0 @@
-import { Scheduler } from "./scheduler";
-export declare class Events {
-    private scheduler;
-    private kafka;
-    constructor(scheduler: Scheduler);
-    run(): Promise<void>;
-}
-//# sourceMappingURL=events.d.ts.map
