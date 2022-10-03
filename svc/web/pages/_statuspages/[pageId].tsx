3dcedd5adc0ed8415bb4c2d2bfa5ea80f86ec5fe
Andreas Thomas <dev@chronark.com>
2022-10-03 23:24:49 +0200

    fix: useSession refetching constantly

--- svc/web/pages/_statuspages/[pageId].tsx
+++ svc/web/pages/_statuspages/[pageId].tsx
@@ -93,8 +93,44 @@ const Row: React.FC<
     );
   };
 export default function Page(
-  { data }: { data: SuperJSONResult },
+  { data }: { data?: SuperJSONResult },
 ) {
+
+
+
+  if (!data) {
+
+    return (
+      <div className="min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+        <div className="mx-auto max-w-max">
+          <main className="sm:flex">
+            <p className="text-4xl font-bold tracking-tight text-primary-600 sm:text-5xl">404</p>
+            <div className="sm:ml-6">
+              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
+                <p className="mt-1 text-base text-gray-500">Please check the URL in the address bar and try again.</p>
+              </div>
+              {/* <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+                <a
+                  href="#"
+                  className="inline-flex items-center rounded-md border border-transparent bg-primary-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
+                >
+                  Go back home
+                </a>
+                <a
+                  href="#"
+                  className="inline-flex items-center rounded-md border border-transparent bg-primary-100 px-4 py-2 text-sm font-medium text-primary-700 hover:bg-primary-200 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
+                >
+                  Contact support
+                </a>
+              </div> */}
+            </div>
+          </main>
+        </div>
+      </div>
+    )
+  }
+
   const page = superjson.deserialize<
     StatusPage & { endpoints: (EndpointType & { checks: Check[] })[] }
   >(data);
@@ -145,7 +181,11 @@ export async function getStaticProps(ctx: GetStaticPropsContext) {
   const pageId = ctx.params?.pageId;
   console.log("Getting static props for page", pageId);
   if (!pageId || Array.isArray(pageId)) {
-    throw new Error("pageId should be string");
+    return {
+      props: {},
+      revalidate: 60,
+    }
+
   }
 
   const db = new PrismaClient();
