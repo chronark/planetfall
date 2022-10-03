04e6e07c80a703326025a6a4f2393524e0cde9cf
Andreas Thomas <dev@chronark.com>
2022-10-04 00:16:20 +0200

    fix: useSession refetching constantly

--- svc/web/middleware.ts
+++ svc/web/middleware.ts
@@ -1,10 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
-
 export const config = {
   matcher: [
-    "/",
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /static (inside /public)
+     * 4. all root files inside /public (e.g. /favicon.ico)
+     */
+    "/((?!api|_next|static|[\\w-]+\\.\\w+).*)",
   ],
 };
+
 
 export default function middleware(req: NextRequest) {
   const url = req.nextUrl;
