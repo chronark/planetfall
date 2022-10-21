7187986ef53e303c68b2d25f8ecb4dd30effeee2
Andreas Thomas <dev@chronark.com>
2022-10-21 14:53:05 +0200

    fix: docs redirects

--- svc/web/next.config.js
+++ svc/web/next.config.js
@@ -41,16 +41,16 @@ const nextConfig = {
       ],
       destination: "/api/rest/:path*",
     },
-    {
-      /**
-       * the docs currently use nextjs zones to run in a different app
-       * It was too hard to make it work in a single app, because it didn't pick up the md files properly
-       */
-      source: "/docs/:path*",
-      destination: `${
-        process.env.DOCS_URL ?? "http://localhost:3001"
-      }/docs/:path*`,
-    },
+    // {
+    //   /**
+    //    * the docs currently use nextjs zones to run in a different app
+    //    * It was too hard to make it work in a single app, because it didn't pick up the md files properly
+    //    */
+    //   source: "/docs/:path*",
+    //   destination: `${
+    //     process.env.DOCS_URL ?? "http://localhost:3001"
+    //   }/docs/:path*`,
+    // },
   ],
 };
 
