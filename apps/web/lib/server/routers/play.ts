38884e2e3d9536da4b7901d3e91b3b1a7e33879b
Andreas Thomas <dev@chronark.com>
2023-01-14 15:46:18 +0100

    feat: add error message

--- apps/web/lib/server/routers/play.ts
+++ apps/web/lib/server/routers/play.ts
@@ -104,7 +104,7 @@ export const playRouter = t.router({
 						console.error(err);
 						throw new TRPCError({
 							code: "INTERNAL_SERVER_ERROR",
-							message: `Unable to ping: ${region.id}: ${err.message}}`,
+							message: `Unable to ping ${region.url}: ${region.id}: ${err.message}}`,
 						});
 					});
 					console.log(
