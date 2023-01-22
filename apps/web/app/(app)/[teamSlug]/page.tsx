cb17b47f999c08aa16be549736385717f64734c9
Andreas Thomas <dev@chronark.com>
2023-01-22 12:38:48 +0100

    deploy

--- apps/web/app/(app)/[teamSlug]/page.tsx
+++ apps/web/app/(app)/[teamSlug]/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-export const dynamic = "force-static";
+export const dynamic = "force-dynamic";
 
 export default async function Page(props: { params: { teamSlug: string } }) {
 	redirect(`/${props.params.teamSlug}/endpoints`);
