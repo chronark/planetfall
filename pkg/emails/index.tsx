4565626c3fcd4b55ac9b04a65087c9ce3d8c5267
Andreas Thomas <dev@chronark.com>
2023-02-07 20:52:47 +0100

    feat: use resend

--- pkg/emails/index.tsx
+++ pkg/emails/index.tsx
@@ -1,36 +1,19 @@
-import { MailService } from "@sendgrid/mail";
 import { render } from "@react-email/render";
+import { Resend } from "resend";
+
+const resend = new Resend("re_Acn8wxyP_LRNZqKNH3urk72y5but6snLT");
 import { EndpointAlert } from "./emails/EndpointAlert";
 export class Email {
-	client: MailService;
+	private client: Resend;
 
 	constructor() {
-		this.client = new MailService();
-		const apikey = process.env.SENDGRID_API_KEY;
-		if (!apikey) {
-			throw new Error("SENDGRID_API_KEY not found");
+		const resendApiKey = process.env.RESEND_API_KEY;
+		if (!resendApiKey) {
+			throw new Error("RESEND_API_KEY not found");
 		}
-		this.client.setApiKey(apikey);
+		this.client = new Resend();
 	}
 
-	public async sendSignInLink(opts: {
-		to: string;
-		link: string;
-		from: string;
-	}): Promise<void> {
-		const res = await this.client.send({
-			from: opts.from,
-			to: opts.to,
-			subject: "Planetfall Sign In",
-			templateId: "d-3894fa3392f94fa385747d9f2944886b",
-			dynamicTemplateData: {
-				link: opts.link,
-			},
-		});
-		if (res[0].statusCode > 300) {
-			throw new Error(`unable to send email. status: ${res[0].statusCode}`);
-		}
-	}
 	public async sendEndpointAlert(opts: {
 		to: string;
 		time: number;
@@ -53,14 +36,11 @@ export class Email {
 				endpointName={opts.endpointName}
 			/>,
 		);
-		const res = await this.client.send({
+		await this.client.sendEmail({
 			from: "chronark@planetfall.io",
 			to: opts.to,
 			subject: "Planetfall Endpoint Alert",
 			html,
 		});
-		if (res[0].statusCode > 300) {
-			throw new Error(`unable to send email. status: ${res[0].statusCode}`);
-		}
 	}
 }
