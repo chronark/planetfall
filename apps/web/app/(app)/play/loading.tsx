040499304f8f0eac884e35628aa2bf5e1042e729
Andreas Thomas <dev@chronark.com>
2023-02-28 23:56:15 +0100

    Update LoadingPage to use flex display

--- apps/web/app/(app)/play/loading.tsx
+++ apps/web/app/(app)/play/loading.tsx
@@ -2,7 +2,7 @@ import { Loading } from "@/components/loading";
 
 export default function LoadingPage() {
 	return (
-		<div className="items-center justify-center w-screen h-screen">
+		<div className="flex items-center justify-center w-screen h-screen">
 			<Loading />
 		</div>
 	);
