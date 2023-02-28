0f611c96a7bcd43b7728424ced29bbe45cfcc371
Andreas Thomas <dev@chronark.com>
2023-02-28 22:48:52 +0100

    Commit message: Update PlayPage to filter regions based on session status

--- apps/web/app/play/page.tsx
+++ apps/web/app/play/page.tsx
@@ -2,7 +2,6 @@ import { getSession } from "@/lib/auth";
 import { db } from "@planetfall/db";
 import { Form } from "./dynamic";
 
-export const dynamic = "force-static";
 export const revalidate = 3600;
 
 export default async function PlayPage(props: {
@@ -13,13 +12,16 @@ export default async function PlayPage(props: {
 		repeat?: string;
 	};
 }) {
-	const { session } = await getSession();
 
-	const regions = session
-		? await db.region.findMany({ where: { visible: true } })
-		: await db.region.findMany({
-				where: { visible: true, platform: "vercelEdge" },
-		  });
+	let regions = await db.region.findMany({
+		where: { visible: true },
+	});
+
+	const { session } = await getSession();
+	if (!session) {
+		regions = regions.filter((r) => r.platform === "vercelEdge");
+	}
+
 	return (
 		<Form
 			regions={regions}
