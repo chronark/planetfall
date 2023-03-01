3ae80f221bbce0ee21f097c605b5d4802756a1ea
Andreas Thomas <dev@chronark.com>
2023-03-01 12:34:04 +0100

    feat: compare play ground

---
+++ apps/web/app/(app)/play/[shareId]/loading.tsx
@@ -0,0 +1,9 @@
+import { Loading } from "@/components/loading";
+
+export default function LoadingPage() {
+	return (
+		<div className="flex items-center justify-center w-screen h-screen">
+			<Loading />
+		</div>
+	);
+}
