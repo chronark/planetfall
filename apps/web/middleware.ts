74eb2678ed5aecd74691e78ff156287b4e7ac752
Andreas Thomas <dev@chronark.com>
2023-03-03 13:55:38 +0100

    wip

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,9 +1,19 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { getAuth, withClerkMiddleware } from "@clerk/nextjs/server";
 
 export const config = {
   matcher: "/((?!_next|_static|_vercel|[\\w-]+\\.\\w+).*)",
+};
+
+
+// Set the paths that don't require the user to be signed in
+const publicPaths = ["/", "/pricing", "/play/*", "/auth/sign-in*", "/auth/sign-up*"];
+
+const isPublic = (path: string) => {
+  return publicPaths.find((x) =>
+    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
+  );
 };
 
 function middleware(req: NextRequest) {
@@ -26,6 +36,20 @@ function middleware(req: NextRequest) {
   if (subdomain !== "") {
     url.pathname = `/_statuspages/${subdomain}`;
     return NextResponse.rewrite(url);
+  }
+
+  if (isPublic(req.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+  // if the user is not signed in redirect them to the sign in page.
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    // redirect the users to /pages/sign-in/[[...index]].ts
+
+    const signInUrl = new URL("/auth/sign-in", req.url);
+    signInUrl.searchParams.set("redirect_url", req.url);
+    return NextResponse.redirect(signInUrl);
   }
   return NextResponse.next();
 }
