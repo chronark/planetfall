83b00e4bdfea53477b46e7cdd8545eb1ed91b2a6
Andreas Thomas <dev@chronark.com>
2023-03-03 13:38:57 +0100

    wip

--- apps/web/app/(app)/auth/sign-in/page.tsx
+++ apps/web/app/(app)/auth/sign-in/page.tsx
@@ -3,7 +3,7 @@ export default function SignInpage() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-tr from-zinc-100 to-white">
-      <SignIn signUpUrl="/auth/sign-up" />
+      <SignIn signUpUrl="/auth/sign-up" redirectUrl="/home" />
     </div>
   );
 }
--- apps/web/app/(app)/auth/sign-up/page.tsx
+++ apps/web/app/(app)/auth/sign-up/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs/app-beta";
 export default function SignUppage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-tr from-zinc-100 to-white">
-      <SignUp signInUrl="/auth/sign-in" />
+      <SignUp signInUrl="/auth/sign-in" redirectUrl="/home" />
     </div>
   );
 }
