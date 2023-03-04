f9457e3e0bc50114deb875345400d22e69fbf068
Andreas Thomas <dev@chronark.com>
2023-03-04 23:07:31 +0100

    feat: debug page (#17)

---
+++ apps/web/app/(app)/debug/user/page.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/button";
+import { getSession } from "@/lib/auth";
+import { db } from "@planetfall/db";
+import { Link } from "lucide-react";
+
+export default async function DebugUserPage() {
+  const { session } = await getSession();
+  if (!session) {
+    return (
+      <div className="flex items-center justify-center w-screen min-h-screen">
+        <Link href="/auth/sign-in">
+          <Button variant="primary" size="lg">
+            Sign in
+          </Button>
+        </Link>
+      </div>
+    );
+  }
+
+  const user = await db.user.findUnique({
+    where: { id: session.user.id },
+    include: {
+      teams: true,
+    },
+  });
+
+  return (
+    <div className="flex items-center justify-center w-screen min-h-screen">
+      <pre className="p-8 m-16 border rounded lg:p-16 bg-zinc-50 border-zinc-300">
+        {JSON.stringify({ session, user }, null, 2)}
+      </pre>
+    </div>
+  );
+}
