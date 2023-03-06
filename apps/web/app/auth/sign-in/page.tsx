b1aad407890d148e148b40a6af0ce60aac8b0a40
Andreas Thomas <dev@chronark.com>
2023-03-07 00:31:47 +0100

    wip

--- apps/web/app/auth/sign-in/page.tsx
+++ apps/web/app/auth/sign-in/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs/app-beta";
 export default function SignInPage() {
   return (
     <div className="flex items-center justify-center w-full min-h-screen">
-      <SignIn path="/auth/sign-in" signUpUrl="/auth/sign-up" redirectUrl="/home" />
+      <SignIn path="/auth/sign-in" signUpUrl="/auth/sign-up"  />
     </div>
   );
 }
