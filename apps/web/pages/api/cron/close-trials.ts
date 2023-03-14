16d26feb15d90b7bd8cc9511e21bf2c76facc0d2
Andreas Thomas <dev@chronark.com>
2023-03-14 23:54:40 +0100

    fix: prevent members accessing billing

--- apps/web/pages/api/cron/close-trials.ts
+++ apps/web/pages/api/cron/close-trials.ts
@@ -21,21 +21,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     },
   });
   for (const team of teams) {
-    if (team.members.length > 1) {
-      console.log("Team", team.id, "has more than one member -> deactivating");
-      // TODO: Send email to team members
-      await db.team.update({
-        where: { id: team.id },
-        data: { plan: "DISABLED" },
-      });
-    } else {
-      // If there's only one member, we can just downgrade it to free
-      console.log("Team", team.id, "has only one member -> downgrading to free");
-      await db.team.update({
-        where: { id: team.id },
-        data: { plan: "FREE" },
-      });
-    }
+    console.log("Team", team.id, "disabling team because trial expired");
+    // TODO: Send email to team members
+    await db.team.update({
+      where: { id: team.id },
+      data: { plan: "DISABLED" },
+    });
   }
   console.log("Deactivated", teams.length, "teams");
   res.status(200).end();
