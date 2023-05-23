b8335c4f6af3bf5152f055a524baa02651a10725
chronark <dev@chronark.com>
2023-05-23 23:29:21 +0200

    chore(trpc.ts): remove console.log and add console.error to errorFormatter function

--- apps/api/lib/trpc/trpc.ts
+++ apps/api/lib/trpc/trpc.ts
@@ -9,16 +9,12 @@ export const t = initTRPC
   .meta<OpenApiMeta>()
   .create({
     errorFormatter: ({ error, shape }) => {
-
+      console.error("[TRPC]", error)
       if (error.code === "INTERNAL_SERVER_ERROR" && process.env.NODE_ENV === "production") {
         return { ...shape, message: "Internal server error" };
       }
-      
-        if (error.code === "NOT_FOUND") {
-          console.log("wtf")
-          return shape
-        }
-      
+
+
       return shape;
     },
   });
